3b396223326147e60143871a63a6b5d1272fc5df
Lorenzo Fontana <fontanalorenzo@me.com>
2014-03-17 03:24:19 +0100

    Initial assetic module support

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -8,10 +8,10 @@
  */
 
 return array(
-    'router' => array(
+    'router'                => array(
         'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
+            'home'        => array(
+                'type'    => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
@@ -25,8 +25,8 @@ return array(
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
             'application' => array(
-                'type'    => 'Literal',
-                'options' => array(
+                'type'          => 'Literal',
+                'options'       => array(
                     'route'    => '/application',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
@@ -34,35 +34,34 @@ return array(
                         'action'        => 'index',
                     ),
                 ),
-                'may_terminate' => true,
-                'child_routes' => array(
+                'may_terminate' => false,
+                'child_routes'  => array(
                     'default' => array(
                         'type'    => 'Segment',
                         'options' => array(
-                            'route'    => '/[:controller[/:action]]',
+                            'route'       => '/[:controller[/:action]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
-                            'defaults' => array(
-                            ),
+                            'defaults'    => array(),
                         ),
                     ),
                 ),
             ),
         ),
     ),
-    'service_manager' => array(
+    'service_manager'       => array(
         'abstract_factories' => array(
             'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
             'Zend\Log\LoggerAbstractServiceFactory',
         ),
-        'aliases' => array(
+        'aliases'            => array(
             'translator' => 'MvcTranslator',
         ),
     ),
-    'translator' => array(
-        'locale' => 'en_US',
+    'translator'            => array(
+        'locale'                    => 'en_US',
         'translation_file_patterns' => array(
             array(
                 'type'     => 'gettext',
@@ -71,67 +70,94 @@ return array(
             ),
         ),
     ),
-    'controllers' => array(
+    'controllers'           => array(
         'invokables' => array(
             'Application\Controller\Index' => 'Application\Controller\IndexController'
         ),
     ),
-    'view_manager' => array(
+    'view_manager'          => array(
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
-        'template_map' => array(
+        'template_map'             => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
-        'template_path_stack' => array(
+        'template_path_stack'      => array(
             __DIR__ . '/../view',
         ),
     ),
 
-    'asset_manager' => array(
-        'resolver_configs'  => array(
-            'collections' => array(
-                '_/asset/js/vendor.js' => array(
-                    '/jquery/jquery.min.js',
-                    '/twbs/bootstrap/dist/js/bootstrap.min.js',
-                ),
-                '_/asset/js/vendor.ie.js' => array(
-                    '/respond/dest/respond.min.js',
-                    '/html5shiv/dist/html5shiv.js',
-                ),
+    'assetic_configuration' => array(
+        'debug'          => true,
+        'buildOnRequest' => true,
+        'webPath'        => 'public/_/assets',
+        'basePath'       => '/_/assets',
+        'default'         => array(
+            'assets' => array(
+                '@application_css',
+                '@application_js'
             ),
-            'paths' => array(
-               __DIR__ . '/../../../public/_/components',
-               __DIR__ . '/../../../vendor',
-            ),
-            'map'   => array(
-//                 '_/asset/js/jquery.min.map' => __DIR__ . '/../../../vendor/components/jquery/jquery.min.map', //TODO check if it's required
-                '_/asset/css/style.css' => __DIR__ . '/../asset/less/style.less',
-                '_/asset/fonts/fontawesome-webfont.eot'  => __DIR__ . '/../../../vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.eot',
-                '_/asset/fonts/fontawesome-webfont.svg'  => __DIR__ . '/../../../vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.svg',
-                '_/asset/fonts/fontawesome-webfont.ttf'  => __DIR__ . '/../../../vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.ttf',
-                '_/asset/fonts/fontawesome-webfont.woff'  => __DIR__ . '/../../../vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.woff',
-                '_/asset/fonts/FontAwesome.otf'  => __DIR__ . '/../../../vendor/fortawesome/font-awesome/fonts/FontAwesome.otf',
-
-            )
         ),
-        'filters'   => array(
-            '_/asset/css/style.css' => array(
-                array('filter' => 'LessphpFilter')
-            )
-        )
-    ),
 
-    // Placeholder for console routes
-    'console' => array(
-        'router' => array(
-            'routes' => array(
+        'modules'        => array(
+            'application' => array(
+                'root_path'   => __DIR__ . '/../assets',
+                'collections' => array(
+                    'application_less' => array(
+                        'assets'  => array(
+                            'less/style.less'
+                        ),
+                        'filters' => array(
+                            'LessphpFilter' => array(
+                                'name' => 'Assetic\Filter\LessphpFilter'
+                            ),
+
+                        ),
+                    ),
+                    'application_fonts' => array(
+                        'assets' => array(
+                            __DIR__ . '/../../../vendor/fortawesome/font-awesome/fonts/*'
+                        ),
+                        'options' => array(
+                            'move_raw' => true,
+                        )
+                    ),
+                    'application_css'  => array(
+                        'assets'  => array(
+                            '@application_less'
+                        ),
+                        'filters' => array(
+                            'CssRewriteFilter' => array(
+                                'name' => 'Assetic\Filter\CssRewriteFilter'
+                            ),
+                            '?CssMinFilter' => array(
+                                'name' => 'Assetic\Filter\CssMinFilter'
+                            )
+
+
+                        ),
+                        'options' => array(
+                            'output' => 'application.css'
+                        )
+                    ),
+                    'application_js'          => array(
+                        'assets' => array(
+                            __DIR__ . '/../../../vendor/components/jquery/jquery.js'
+                        )
+                    ),
+                ),
             ),
+        ),
+    ),
+    // Placeholder for console routes
+    'console'               => array(
+        'router' => array(
+            'routes' => array(),
         ),
     ),
 );
